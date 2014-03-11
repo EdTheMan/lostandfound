e12fc58305a736bfc952b84a8836a99160904352
EdTheMan <chiieong@brandeis.edu>
2014-03-11 09:08:58 -0400

    Completed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lostandfound::Application.config.secret_key_base = 'd80e0825bbfc476c4fc1be34b5e2b5c05dcf2235cdaec361af01be6792b3257406df036f7f4464975e0fb6dc1d4e0b73206767408d4e86ff78488cba0674faf2'
